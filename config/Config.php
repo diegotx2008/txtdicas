e1aa2c2b7a0a2e5c4508513b64871a0e244ce498
Diego Teixeira De Almeida <diegotx2008@gmail.com>
2022-06-10 20:46:07 -0400

    Subindo arquivos iniciais do site do canal.

---
+++ config/Config.php
@@ -0,0 +1,5 @@
+<?php
+
+define('URL','../../../../../var/www/html/txtdicas/');
+define('APP','../../../../../var/www/html/txtdicas/app/');
+define('PUBLIC','../../../../../var/www/html/txtdicas/Public/');
